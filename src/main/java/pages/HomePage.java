a2913ab5f39b575f37975819d102a34e689be747
Stephen George <stephen.george@ibm.com>
2021-03-17 22:50:48 -0500

    - building test scenario

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,8 +1,11 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class HomePage extends BasePage {
 
@@ -12,89 +15,124 @@ public class HomePage extends BasePage {
     @FindBy(id = "weigh")
     private WebElement weighButton;
 
-    @FindBy(id = "coin_0")
-    private WebElement coin0;
+    @FindBy(xpath = "//body/div[@id='root']/div[1]/div[1]/div[5]")
+    private WebElement weighings;
 
-    @FindBy(id = "coin_1")
-    private WebElement coin1;
-
-    @FindBy(id = "coin_2")
-    private WebElement coin2;
-
-    @FindBy(id = "coin_3")
-    private WebElement coin3;
-
-    @FindBy(id = "coin_4")
-    private WebElement coin4;
-
-    @FindBy(id = "coin_5")
-    private WebElement coin5;
-
-    @FindBy(id = "coin_6")
-    private WebElement coin6;
-
-    @FindBy(id = "coin_7")
-    private WebElement coin7;
-
-    @FindBy(id = "coin_8")
-    private WebElement coin8;
-
-    @FindBy(id = "left_0")
-    private WebElement left0;
-
-    @FindBy(id = "left_1")
-    private WebElement left1;
-
-    @FindBy(id = "left_2")
-    private WebElement left2;
-
-    @FindBy(id = "left_3")
-    private WebElement left3;
-
-    @FindBy(id = "left_4")
-    private WebElement left4;
-
-    @FindBy(id = "left_5")
-    private WebElement left5;
-
-    @FindBy(id = "left_6")
-    private WebElement left6;
-
-    @FindBy(id = "left_7")
-    private WebElement left7;
-
-    @FindBy(id = "left_8")
-    private WebElement left8;
-
-    @FindBy(id = "right_0")
-    private WebElement right0;
-
-    @FindBy(id = "right_1")
-    private WebElement right1;
-
-    @FindBy(id = "right_2")
-    private WebElement right2;
-
-    @FindBy(id = "right_3")
-    private WebElement right3;
-
-    @FindBy(id = "right_4")
-    private WebElement right4;
-
-    @FindBy(id = "right_5")
-    private WebElement right5;
-
-    @FindBy(id = "right_6")
-    private WebElement right6;
-
-    @FindBy(id = "right_7")
-    private WebElement right7;
-
-    @FindBy(id = "right_8")
-    private WebElement right8;
+//    @FindBy(id = "coin_0")
+//    private WebElement coin0;
+//
+//    @FindBy(id = "coin_1")
+//    private WebElement coin1;
+//
+//    @FindBy(id = "coin_2")
+//    private WebElement coin2;
+//
+//    @FindBy(id = "coin_3")
+//    private WebElement coin3;
+//
+//    @FindBy(id = "coin_4")
+//    private WebElement coin4;
+//
+//    @FindBy(id = "coin_5")
+//    private WebElement coin5;
+//
+//    @FindBy(id = "coin_6")
+//    private WebElement coin6;
+//
+//    @FindBy(id = "coin_7")
+//    private WebElement coin7;
+//
+//    @FindBy(id = "coin_8")
+//    private WebElement coin8;
+//
+//    @FindBy(id = "left_0")
+//    private WebElement left0;
+//
+//    @FindBy(id = "left_1")
+//    private WebElement left1;
+//
+//    @FindBy(id = "left_2")
+//    private WebElement left2;
+//
+//    @FindBy(id = "left_3")
+//    private WebElement left3;
+//
+//    @FindBy(id = "left_4")
+//    private WebElement left4;
+//
+//    @FindBy(id = "left_5")
+//    private WebElement left5;
+//
+//    @FindBy(id = "left_6")
+//    private WebElement left6;
+//
+//    @FindBy(id = "left_7")
+//    private WebElement left7;
+//
+//    @FindBy(id = "left_8")
+//    private WebElement left8;
+//
+//    @FindBy(id = "right_0")
+//    private WebElement right0;
+//
+//    @FindBy(id = "right_1")
+//    private WebElement right1;
+//
+//    @FindBy(id = "right_2")
+//    private WebElement right2;
+//
+//    @FindBy(id = "right_3")
+//    private WebElement right3;
+//
+//    @FindBy(id = "right_4")
+//    private WebElement right4;
+//
+//    @FindBy(id = "right_5")
+//    private WebElement right5;
+//
+//    @FindBy(id = "right_6")
+//    private WebElement right6;
+//
+//    @FindBy(id = "right_7")
+//    private WebElement right7;
+//
+//    @FindBy(id = "right_8")
+//    private WebElement right8;
 
     public HomePage(WebDriver driver) {
         super(driver);
         driver.get("http://ec2-54-208-152-154.compute-1.amazonaws.com/");
+    }
+
+    public void insertCoinIntoLeftGrid(int coin, int cell) {
+        WebElement coinToAdd = driver.findElement(By.id("coin_" + coin));
+        String coinText = coinToAdd.getText();
+        WebElement cellToBeFilled = driver.findElement(By.id("left_" + cell));
+
+        cellToBeFilled.sendKeys(coinText);
+    }
+
+    public void insertCoinIntoRightGrid(int coin, int cell) {
+        WebElement coinToAdd = driver.findElement(By.id("coin_" + coin));
+        String coinText = coinToAdd.getText();
+        WebElement cellToBeFilled = driver.findElement(By.id("right_" + cell));
+
+        cellToBeFilled.sendKeys(coinText);
+    }
+
+    public void pressWeighButton() {
+        weighButton.click();
+    }
+
+    public void checkLatestWeighResult() {
+        List<WebElement> list = weighings.findElements(By.tagName("li"));
+        int count = list.size();
+
+        //TODO: get latest once in the list
+        for (WebElement element : list) {
+            System.out.println(element.getText());
+        }
+        System.out.println(count);
+
     }
 }
--- src/test/java/stepDefinitions/HomePageStepDefinitions.java
+++ src/test/java/stepDefinitions/HomePageStepDefinitions.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import pages.HomePage;
@@ -21,5 +22,45 @@ public class HomePageStepDefinitions extends BaseTest {
 
         homePage = new HomePage(driver);
 //        Assertions.assertTrue(homePage.isAtHomePage());
+    }
+
+    @When("coin 0 is added to first cell in the left grid")
+    public void inputCoinZeroIntoLeftGrid() {
+        homePage.insertCoinIntoLeftGrid(0,0);
+    }
+
+    @When("coin 1 is added to second cell in the left grid")
+    public void inputCoinOneIntoLeftGrid() {
+        homePage.insertCoinIntoLeftGrid(1,1);
+    }
+
+    @When("coin 2 is added to third cell in the left grid")
+    public void inputCoinTwoIntoLeftGrid() {
+        homePage.insertCoinIntoLeftGrid(2,2);
+    }
+
+    @When("coin 3 is added to first cell in the right grid")
+    public void inputCoinThreeIntoRightGrid() {
+        homePage.insertCoinIntoRightGrid(3,0);
+    }
+
+    @When("coin 4 is added to second cell in the right grid")
+    public void inputCoinFourIntoRightGrid() {
+        homePage.insertCoinIntoRightGrid(4,1);
+    }
+
+    @When("coin 5 is added to third cell in the right grid")
+    public void inputCoinFiveIntoRightGrid() {
+        homePage.insertCoinIntoRightGrid(5,2);
+    }
+
+    @When("weigh button is pressed")
+    public void pressWeighButton() {
+        homePage.pressWeighButton();
+    }
+
+    @When("latest weight result is checked")
+    public void checkLatestWeightResult() {
+        homePage.checkLatestWeighResult();
     }
 }
